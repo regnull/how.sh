99ec460ea41b82e1bfd933a2443eefb0a76f4b7b
Leonid Gorkin <regnull@gmail.com>
2025-01-22 17:17:24 -0500

    example

---
+++ examples/bob.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+../how.sh -y create a file named bob which contains the current timestamp 
+../how.sh -y tell me what does file named bob contain
+../how.sh -y delete file named bob
